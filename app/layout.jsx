142c570c70b6e13d3bf2be4739830b7e80e8ece1
Prejithpcet <prejithp2002@gmail.com>
2023-08-01 14:09:08 +0530

    First Commit

---
+++ app/layout.jsx
@@ -0,0 +1,29 @@
+//This layout files applies to every other pages
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "Promptopia",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
